353e35b9a2e865588f7c93299e4953cf4bd29f06
inger_na <inger_na@mail.ru>
2021-09-28 10:15:43 +0200

    Exercises Done

--- Exercises/3-unique.js
+++ Exercises/3-unique.js
@@ -3,6 +3,12 @@
 // Create and return a new array without duplicate elements
 // Don't modify initial array
 
-const unique = (array) => [];
+const unique = (array) => {
+  const result = [];
+  for (const item of array) {
+    if (!result.includes(item)) result.push(item);
+  }
+  return result;
+};
 
 module.exports = { unique };
